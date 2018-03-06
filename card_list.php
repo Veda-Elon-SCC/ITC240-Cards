2fec3b92fc0394e6a26f12c1c985eb71caf383a8
Veda Elon <veda.elon@gmail.com>
2018-03-05 18:58:30 -0800

    pager addition

--- card_list.php
+++ card_list.php
@@ -6,6 +6,11 @@
 <html lang="en">
 
 <?php include 'includes/head.php';?>
+    
+<?php include 'includes/Pager.php';?>
+    
+$config->loadhead .= '<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>';
+
 <body>
 <?php get_header()?>
 
@@ -17,11 +22,20 @@ $sql = "select * from Cards";
 //we connect to the db here
 $iConn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
-//we extract the data here
-$result = mysqli_query($iConn,$sql);
+$first = '<i class="far fa-arrow-alt-circle-left"></i>';    
+$prev = '<i class="fas fa-arrow-left"></i>';
+$next = '<i class="fas fa-arrow-right"></i>';
+$last = '<i class="far fa-arrow-alt-circle-right"></i>';
+    
+$myPager = new Pager(16,$first,$prev,$next,$last);
+$sql = $myPager->loadSQL($sql,$iConn);  #load SQL, pass in existing connection, add offset
+$result = mysqli_query($iConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($iConn)));
 
 if(mysqli_num_rows($result) > 0)
 {//show records
+    
+    echo $myPager->showNAV();
+    if($myPager->showTotal()==1){$itemz = "customer";}else{$itemz = "customers";}
 
     while($row = mysqli_fetch_assoc($result))
     {
@@ -37,6 +51,8 @@ if(mysqli_num_rows($result) > 0)
         echo '</p>';
         echo '</div>';
     }
+    
+    echo $myPager->showNAV();
 
 }else{//inform there are no records
     echo '<p>There are currently no customers</p>';
